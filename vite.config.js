4ce0dfc92291f41fb5512dbbe4e0cb344b83b88f
nj jdust2mertel-code <dust2mertel@gmail.com>
2026-02-25 07:38:21 +0200

    fix vite config defineConfig import

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig(({ command }) => ({
   base: command === "serve" ? "/" : "/FadiezPC/",
   plugins: [react()],
